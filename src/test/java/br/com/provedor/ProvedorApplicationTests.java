3c06cef97c2c81a8e63b7f1cb94a6016513f27b8
Lucas <lucasfarias50@outlook.com.br>
2019-05-23 16:34:26 -0300

    Primeira entrega

---
+++ src/test/java/br/com/provedor/ProvedorApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.provedor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProvedorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
